88a9463d5ad23fbbe9902de8d1ea0210df2caf90
Mikhail Bondarenko <m@bnd.su>
2022-09-17 22:43:49 +0000

    style: prettier some files

--- libs/destroy-module-demo/src/app/b-module/b.component.ts
+++ libs/destroy-module-demo/src/app/b-module/b.component.ts
@@ -1,11 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { LoopService } from '../services/loop.service';
 
-@Component({
-  selector: `dm-b`,
-  templateUrl: `./b.component.html`,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
+@Component({ selector: `dm-b`, templateUrl: `./b.component.html`, changeDetection: ChangeDetectionStrategy.OnPush })
 export class BComponent {
   constructor(private readonly _loopService: LoopService) {}
 }
