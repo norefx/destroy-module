0d68b963c01d46018ce4423bc54261cd770521a5
Mikhail Bondarenko <m@bnd.su>
2022-09-22 03:57:36 +0000

    fix: remove unnecessary imports

--- libs/destroy-module/src/types/defined-object.util.ts
+++ libs/destroy-module/src/types/defined-object.util.ts
@@ -1,6 +1,3 @@
-import { isDestroyableModule } from './destroyable-module.util';
-import { LoadedConfig } from './loaded-config.type';
-
 export function isDefinedObject<T>(value: any): value is Object {
   return !!value && value instanceof Object;
 }
