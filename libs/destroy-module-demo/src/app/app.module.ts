428f156e6d4d2a06154e3efb106955e9e8405328
Mikhail Bondarenko <m@bnd.su>
2022-09-23 15:54:56 +0000

    build: change module alias

--- libs/destroy-module-demo/src/app/app.module.ts
+++ libs/destroy-module-demo/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { DestroyModuleGuard } from 'ngx-destroy-module';
+import { DestroyModuleGuard } from '@norefx/ngx-destroy-module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
 
