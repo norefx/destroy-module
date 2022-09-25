60fb41b9b65dae45126f0f6c6aad86c2d7e91b5b
Mikhail Bondarenko <m@bnd.su>
2022-09-25 14:32:01 +0000

    fix: route with extended config type

--- libs/destroy-module/src/types/route-with-config.type.ts
+++ libs/destroy-module/src/types/route-with-config.type.ts
@@ -2,4 +2,4 @@ import { ActivatedRouteSnapshot } from '@angular/router';
 import { ExtendedRoute } from './extended-route.type';
 
 export type RouteWithConfig = Pick<ActivatedRouteSnapshot, 'routeConfig'>;
-export type RouteWithExtendedConfig<T> = RouteWithConfig & { routeConfig: ExtendedRoute<T> };
+export type RouteWithExtendedConfig<T> = RouteWithConfig | { routeConfig: ExtendedRoute<T> };
