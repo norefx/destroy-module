76349e52ec2031a744f85bf2ec0443b095a6f794
Mikhail Bondarenko <m@bnd.su>
2022-10-02 11:14:29 +0000

    ci(jest): fix ng jest runner

--- libs/destroy-module-demo/jest.app.config.js
+++ libs/destroy-module-demo/jest.app.config.js
@@ -1,7 +1,1 @@
-module.exports = {
-  ...require(`./jest.config`),
-  testMatch: [
-    '**/libs/destroy-module-demo/**/__tests__/**/*.+(ts|js)?(x)',
-    '**/libs/destroy-module-demo/**/+(*.)+(spec|test).+(ts|js)?(x)',
-  ],
-};
+module.exports = { ...require(`./jest.config`) };
