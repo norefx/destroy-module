fb05bcb6f3b6e003a0fdb8c125dddcb589da7168
Mikhail Bondarenko <m@bnd.su>
2022-10-05 20:25:52 +0000

    ci(jest): + fix jest configs

--- libs/destroy-module/jest.lib.config.js
+++ libs/destroy-module/jest.lib.config.js
@@ -1,1 +1,5 @@
-module.exports = { ...require(`./jest.config`) };
+module.exports = {
+  ...require(`./jest.config`),
+  globals: { 'ts-jest': { tsConfig: `<rootDir>tsconfig.spec.json` } },
+  setupFilesAfterEnv: [`<rootDir>../../jest.setup.ts`],
+};
