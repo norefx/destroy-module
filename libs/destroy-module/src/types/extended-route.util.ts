f36af0d441fa7375347898265d8e37dad62847a7
Mikhail Bondarenko <m@bnd.su>
2022-10-08 07:40:44 +0000

    refactor: optional `ExtendedRoute` type

--- libs/destroy-module/src/types/extended-route.util.ts
+++
@@ -1,7 +0,0 @@
-import { ExtendedRoute } from './extended-route.type';
-import { hasField } from './has-field.util';
-import { isLoadedConfig } from './loaded-config.util';
-
-export function isExtendedRoute<T>(value: any): value is ExtendedRoute<T> {
-  return hasField(`_loadedConfig`)(value) && isLoadedConfig(value._loadedConfig);
-}
