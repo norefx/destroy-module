92674dff8daaaa575fcd971bb91134fd39900f1d
Mikhail Bondarenko <m@bnd.su>
2022-10-08 07:40:44 +0000

    refactor: optional `ExtendedRoute` type

--- libs/destroy-module/src/types/extended-route.type.ts
+++ libs/destroy-module/src/types/extended-route.type.ts
@@ -1,10 +1,6 @@
 import { Route } from '@angular/router';
 import { LoadedConfig } from './loaded-config.type';
-import { PartialPickField } from './partial-pick.type';
 
 export interface ExtendedRoute<T> extends Route {
-  _loadedConfig: LoadedConfig<T>;
+  _loadedConfig?: LoadedConfig<T>;
 }
-
-// eslint-disable-next-line quotes
-export type PartialExtendedRoute<T> = PartialPickField<ExtendedRoute<T>, '_loadedConfig'>;
