3b58b51764e8a499949f8d66ccef5597331efa53
Mikhail Bondarenko <m@bnd.su>
2022-10-08 07:40:44 +0000

    refactor: optional `ExtendedRoute` type

--- libs/destroy-module/src/types/extended-route.type.ts
+++ libs/destroy-module/src/types/extended-route.type.ts
@@ -2,7 +2,5 @@ import { Route } from '@angular/router';
 import { LoadedConfig } from './loaded-config.type';
 
 export interface ExtendedRoute<T> extends Route {
-  _loadedConfig: LoadedConfig<T>;
+  _loadedConfig?: LoadedConfig<T>;
 }
-
-export type PartialExtendedRoute<T> = Partial<ExtendedRoute<T>>;
