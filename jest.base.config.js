8fe5b844ab74bdac3d5a5c66cdcaa56132c7859a
Mikhail Bondarenko <m@bnd.su>
2022-10-09 07:34:29 +0000

    style(jest): prettier `jest.base.config`

--- jest.base.config.js
+++ jest.base.config.js
@@ -1,5 +1,2 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  ...require(`./jest.config`),
-  globals: { 'ts-jest': { tsconfig: `<rootDir>tsconfig.spec.json` } },
-};
+module.exports = { ...require(`./jest.config`), globals: { 'ts-jest': { tsconfig: `<rootDir>tsconfig.spec.json` } } };
